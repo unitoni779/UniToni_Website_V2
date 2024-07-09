044634fb241dfa47efe5f0dc874ac7cefb65b7ab
boss4edu <boss4edu@gmail.com>
2024-07-09 23:42:14 +0300

    update

--- src/hero.js
+++ src/hero.js
@@ -1,30 +1,36 @@
 import React from 'react';
-import tasarim from './heroo.png'
+import tasarim from './heroo.png';
+import app from './app2.png';
+import google from './google.png';
 
 const ShuffleHero = () => {
     return (
-        <section className="sm:mt-6 lg:mt-8 mt-12 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <section className="sm:mt-6 lg:mt-8 mt-12 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-4">
             <div className="my-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 flex gap-3 lg:flex-justify lg:flex flex-col lg:flex-row">
                 <div className="sm:text-center lg:text-left">
                     <h1 className="text-4xl tracking-tight font-extrabold text-gray-800 sm:text-5xl md:text-6xl">
-                        <span className="block xl:inline">Data to enrich your</span>
-                        <span className="block text-indigo-600 xl:inline">online business</span>
+                        <span className="block xl:inline">The Future of Education is </span>
+                        <span className="block text-indigo-600 xl:inline" style={{ color: "#ff9a24" }}>Here</span>
                     </h1>
                     <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                        Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+                        UniToni is easy modern, saves time and combines 3 systems into one platform.
                     </p>
 
                     <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                        <div className="rounded-md shadow">
-                            <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-gray-600 md:py-4 md:text-lg md:px-10">
+                        <div className="rounded-md">
+                            <a
+                                href="#"
+                                className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-bold rounded-full text-white bg-gray-800 hover:bg-gray-600 md:py-4 md:text-lg md:px-10"
+                                style={{ backgroundColor: "#0087f7" }}
+                            >
                                 Get started
                             </a>
                         </div>
-                        <div className="mt-3 sm:mt-0 sm:ml-3">
-                            <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-gray-800 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
-                                Live demo
-                            </a>
-                        </div>
+                    </div>
+
+                    <div className="mt-4 flex justify-center sm:justify-start gap-3">
+                        <img className="h-16 w-auto sm:h-24 sm:w-auto lg:h-32 lg:w-auto object-cover" src={app} alt="App Button"style={{width:"200px"}} />
+                        <img className="h-16 w-auto sm:h-24 sm:w-auto lg:h-32 lg:w-auto object-cover" src={google} alt="Google Button" style={{width:"200px"}}/>
                     </div>
                 </div>
 
--- src/nav.js
+++ src/nav.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import logo from './logo.png'
+import logo from './logo.png';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -13,21 +13,26 @@ const Navbar = () => {
       <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4 relative">
         <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src={logo} className="h-8" alt="Flowbite Logo" />
-     
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <button
-            type="button"
-            className="hidden md:inline-block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mr-5"
-          >
-           Login
-          </button>
-          <button
-            type="button"
-            className="hidden md:inline-block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
-          >
-        Book Demo
-          </button>
+        <a
+          href="#"
+          className="hidden md:inline-block text-black hover:text-blue-800 focus:outline-none font-bold text-md mr-7 cursor-pointer mt-2"
+          style={{color:"#ff9a24"}}
+       >
+          Login
+        </a>
+
+        <button 
+        type="button"
+        className="text-white bg-blue-700 hover:bg-blue-800 hidden md:block focus:outline-none font-bold focus:ring-4 focus:ring-blue-300 rounded-full text-md px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        style={{backgroundColor: "#0087f7"}}
+      >
+       Book Demo
+      </button>
+
+
+
           <button
             onClick={toggleMenu}
             type="button"
@@ -37,7 +42,7 @@ const Navbar = () => {
           >
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
             </svg>
           </button>
         </div>
@@ -47,33 +52,28 @@ const Navbar = () => {
         >
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 font-bold">Home</a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 font-bold">About</a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 font-bold">Services</a>
             </li>
             <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 font-bold">Contact</a>
             </li>
             <li>
-              <button
-                type="button"
-                className="block py-2 px-3 text-white bg-blue-700 rounded md:hidden hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium mt-4 md:mt-0"
-              >
-                Get started
-              </button>
-            </li>
-            <li>
-              <button
-                type="button"
-                className="block py-2 px-3 text-white bg-blue-700 rounded md:hidden hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium mt-4 md:mt-0"
-              >
-                Get started
-              </button>
-            </li>
+            <a href="#" className="block md:hidden py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white font-bold">
+              Login
+            </a>
+          </li>
+          <li>
+            <a href="#" className="block md:hidden py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white font-bold">
+              Book Demo
+            </a>
+          </li>
+
           </ul>
         </div>
       </div>
