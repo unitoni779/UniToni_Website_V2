f4da61b9643151b20fa1e15088c6a5ee319f07f3
boss4edu <boss4edu@gmail.com>
2024-08-26 22:16:16 +0300

    upadte

--- src/Components/HorizontalScroll/HorizontalScroll.jsx
+++ src/Components/HorizontalScroll/HorizontalScroll.jsx
@@ -157,7 +157,7 @@ const HorizontalScroll = () => {
                         style={{ 
                             opacity: secondTextVisibility,
                             position: 'fixed', // Ensure it's visible regardless of scroll
-                            top: '13%', // Adjusted position
+                            top: '14%', // Adjusted position
                             left: '50%',
                             transform: 'translateX(-50%)',
                             width: '80%',
