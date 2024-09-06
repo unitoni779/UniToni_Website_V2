58e4b16f9d2ed6897f457a27e977218c283fc7b2
boss4edu <boss4edu@gmail.com>
2024-09-07 01:28:06 +0300

    update

--- src/Components/HorizontalScroll/HorizontalScroll.jsx
+++ src/Components/HorizontalScroll/HorizontalScroll.jsx
@@ -105,7 +105,7 @@ const HorizontalScroll = () => {
                     <motion.div
                         className="firstImageWrapper"
                         style={{ y: yFirstImage }}
-                        transition={{ type: 'spring', stiffness: 300, damping: 25, ease: 'easeInOut' }}
+                        transition={{ type: 'spring', stiffness: 200, damping: 50, ease: 'easeInOut' }}
                     >
                         {/* First Image and its text */}
                         <motion.div
@@ -123,7 +123,7 @@ const HorizontalScroll = () => {
                                 fontWeight: '900',
                                 textShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)',
                             }}
-                            transition={{ type: 'spring', stiffness: 300, damping: 25, ease: 'easeInOut' }}
+                            transition={{ type: 'spring', stiffness: 200, damping: 50, ease: 'easeInOut' }}
                         >
                             Connect & Engage {isMobile ? <br /> : null} Campus Life
                         </motion.div>
@@ -131,7 +131,7 @@ const HorizontalScroll = () => {
                         <motion.div
                             className="firstImage"
                             style={{ scale: scaleFirstImage }}
-                            transition={{ type: 'spring', stiffness: 300, damping: 25, ease: 'easeInOut' }}
+                            transition={{ type: 'spring', stiffness: 200, damping: 50, ease: 'easeInOut' }}
                         >
                             <ImageContainer
                                 imageSource={require('../../Screens/s1.png')}
@@ -189,7 +189,7 @@ const HorizontalScroll = () => {
                             initial={{ opacity: 0, y: 20 }}
                             animate={{ opacity: 1, y: 0 }}
                             exit={{ opacity: 0, y: -20 }}
-                            transition={{ type: 'spring', stiffness: 300, damping: 25, ease: 'easeInOut' }}
+                            transition={{ type: 'spring', stiffness: 200, damping: 50, ease: 'easeInOut' }}
                             dangerouslySetInnerHTML={{ __html: text }}
                         />
                     )}
