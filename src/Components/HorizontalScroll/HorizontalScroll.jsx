eb396d8f568aaf8ca4b574d77bcf0a24d1017549
boss4edu <boss4edu@gmail.com>
2024-09-07 01:36:29 +0300

    upadte

--- src/Components/HorizontalScroll/HorizontalScroll.jsx
+++ src/Components/HorizontalScroll/HorizontalScroll.jsx
@@ -19,13 +19,11 @@ const HorizontalScroll = () => {
     const scrollSpeedFactor = isMobile ? 0.3 : 1;
 
     // X-axis transform for horizontal scrolling based on device type
-   // X-axis transform for horizontal scrolling based on device type
-   const x = useTransform(
-    scrollYProgress,
-    [0.2, 1.5],  // Adjust the range for more gradual scrolling
-    isMobile ? ["10%", `-${400 * scrollSpeedFactor}%`] : ["10%", `-${150 * scrollSpeedFactor}%`]
-);
-
+    const x = useTransform(
+        scrollYProgress,
+        [0.3, 1],
+        isMobile ? ["10%", `-$245 * scrollSpeedFactor}%`] : ["10%", `-${70 * scrollSpeedFactor}%`]
+    );
 
     // Y-axis transform for the first image, starts higher and moves down smoothly
     const yFirstImage = useTransform(scrollYProgress, [0, 0.25], ["-150px", "0px"]);
